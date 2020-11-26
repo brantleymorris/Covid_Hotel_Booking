06b4ef3b9e64cc51b78236f2769daca78a3b9181
Calvin Carter <calvincarter@pingidentity.com>
2020-11-25 20:06:50 -0600

    add hotel suggestions API

--- Assets/bookingScript.js
+++ Assets/bookingScript.js
@@ -1,51 +1,61 @@
-var settings = {
+var hotelSuggestions = $("#suggested-hotels");
+
+const hotelSettings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://hotels4.p.rapidapi.com/properties/get-hotel-photos?id=1178275040",
 	"method": "GET",
 	"headers": {
+		"x-rapidapi-key": "d1b2086cbfmshc413b7cedb10ce8p18aa2ajsn8809ec199a93",
 		"x-rapidapi-host": "hotels4.p.rapidapi.com",
-		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
 	}
-}
+};
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+var city = "San Francisco";
+hotelSettings.url = "https://hotels4.p.rapidapi.com/locations/search?query=" + city + "&locale=en_US";
+
+$.ajax(hotelSettings).done(function (response) {
+	var suggestions = response.suggestions;
+
+	var hotelGroups = suggestions.find(function (element) {
+		return element.group === "HOTEL_GROUP"
+	});
+
+	if (hotelGroups && hotelGroups.entities.length === 0) return;
+
+	var hotels = hotelGroups.entities.map(function (element) {
+		return {
+			caption: element.caption,
+			name: element.name,
+			id: element.destinationId
+		}
+	});
+
+	hotels.forEach(function (hotel) {
+		hotelSettings.url = "https://hotels4.p.rapidapi.com/properties/get-hotel-photos?id=" + hotel.id;
+
+		$.ajax(hotelSettings).done(function (response) {
+			var hotelImages = response.hotelImages;
+
+			console.log(hotel.name);
+			var hotelImage = hotelImages[0].baseUrl.replace("{size}", "z");
+
+		
+			var cardWrapper = $("<div>").attr("class", "mdl-cell mdl-cell--4-col");
+			var card = $("<div>").attr("class", "demo-card-wide mdl-card mdl-shadow--2dp");
+
+			var titleWrapper = $("<div>").attr("class", "mdl-card__title").css("background-image", "url(" + hotelImage + ")");
+
+			
+			var title = $("<h2>").attr("class", "mdl-card__title-text").text(hotel.name);
+			titleWrapper.append(title);
+
+			var caption = $("<div>").attr("class", "mdl-card__supporting-text").text("Lorem ipsum dolor sit amet");
+
+			card.append(titleWrapper, caption);
+			cardWrapper.append(card);
+
+			hotelSuggestions.append(cardWrapper);
+		});
+	})
+
 });
-
-
-// var cityName;
-// var getCity = function(cityName) {
-//      var city = $("#city");
-$.ajax(settings).then(function (response) {
-	// console.log(url);
-	// var suggestions_entitiesLength = response.suggestions[3].entities.length;
-	// var hotelgroup_suggestions = response.suggestions[3];
-	console.log(response);
-	console.log(response.term);
-	for(var i=0;i<response.suggestions[3].entities.length;i++){
-		console.log(JSON.stringify(response.suggestions[3].entities[i].name));
-
-
-	}
-
-
-
-
-		// var city = $("#city");
-	// var currentName = response.term;
-	// var image = (`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
-	// h2El.append(`${currentName} ${image}`);
-	// var result = response.main;
-	// // Appending the values to the elements
-	// city.append($("<p>").text(`Humidity:${result.humidity}%`));
-	// city.append($("<p>").text(`Temperature:${result.temp}°F`));
-	// city.append($("<p>").text(`Wind Speed:${response.wind.speed}MPH`));
-
-});
-// }
-// $(".button").on("click", function (event) {
-//     event.preventDefault();
-//     var input_cityEl = $("#input_city").val();
-//     getCity(input_cityEl);
-// });
