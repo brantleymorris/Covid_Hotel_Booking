79b8f4918ab548e9a092ff7f85eb67e844d6c2c1
BPM <brantleymorris@me.com>
2020-11-30 09:22:52 -0600

    integrate covid api, still need to write info to dom

--- Assets/bookingScript.js
+++ Assets/bookingScript.js
@@ -5,7 +5,7 @@ const hotelSettings = {
 	"crossDomain": true,
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": "d1b2086cbfmshc413b7cedb10ce8p18aa2ajsn8809ec199a93",
+		"x-rapidapi-key": "a94a55b6bbmsh2098aa9a74c64eap1cd01ejsn1958536b713c",
 		"x-rapidapi-host": "hotels4.p.rapidapi.com",
 	}
 };
@@ -16,7 +16,12 @@ function getCityInfo (city) {
 hotelSettings.url = "https://hotels4.p.rapidapi.com/locations/search?query=" + city + "&locale=en_US";
 
 $.ajax(hotelSettings).done(function (response) {
+	console.log(response);
 	var suggestions = response.suggestions;
+	var lon = response.suggestions[0].entities[0].longitude;
+	console.log(lon);
+	var lat = response.suggestions[0].entities[0].latitude;
+	console.log(lat);
 
 	var hotelGroups = suggestions.find(function (element) {
 		return element.group === "HOTEL_GROUP"
@@ -74,21 +79,80 @@ $.ajax(hotelSettings).done(function (response) {
 			console.log(failReason);
 		});
 	
-	var lon = // need to drill down
-	var lat = // need to drill down
-	var geoAPI = "XBCZUOECX4F5A1OWBDCJAALNMLNZHDXM";
-	
-	var geoidURL = "https://geodatasource-geodatasource-location-search-web-service-v1.p.rapidapi.com/city?key=" + geoAPI + "&lat=" + lat + "&lng=" + lon;
-	
+	// put covid and location api here
+	var fccAPI = "https://geo.fcc.gov/api/census/area?lat=" + lat + "&lon=" + lon + "&format=json";
+
 	$.ajax({
-		url: geoidURL,
+		url: fccAPI,
 		method: "GET"
 	}).done(function (response) {
 		console.log(response);
 
-		var state = // drill down
-		var stateQueryURL = "https://api.covidtracking.com/v1/states/" + state + "/current.json";
-		
+		var county = response.results[0].county_name
+		var state = response.results[0].state_name
+
+		var stateParsed = "Blank";
+		var states = [
+			{name: "Alabama", code: "al"},
+			{name: "Alaska", code: "ak"},
+			{name: "Arizona", code: "az"},
+			{name: "California", code: "ca"},
+			{name: "Colorado", code: "co"},
+			{name: "Connecticut", code: "ct"},
+			{name: "Delaware", code: "de"},
+			{name: "Florida", code: "fl"},
+			{name: "Georgia", code: "ga"},
+			{name: "Hawaii", code: "hi"},
+			{name: "Idaho", code: "id"},
+			{name: "Illinois", code: "il"},
+			{name: "Indiana", code: "in"},
+			{name: "Iowa", code: "ia"},
+			{name: "Kansas", code: "ks"},
+			{name: "Kentucky", code: "ky"},
+			{name: "Louisiana", code: "la"},
+			{name: "Maine", code: "me"},
+			{name: "Maryland", code: "md"},
+			{name: "Massachusetts", code: "ma"},
+			{name: "Michigan", code: "mi"},
+			{name: "Minnesota", code: "mn"},
+			{name: "Mississippi", code:"ms" },
+			{name: "Missouri", code: "mo"},
+			{name: "Montana", code: "mt"},
+			{name: "Nebraska", code: "ne"},
+			{name: "Nevada", code: "nv"},
+			{name: "New Hampshire", code: "nh"},
+			{name: "New Jersey", code: "nj"},
+			{name: "New Mexico", code: "nm"},
+			{name: "New York", code: "ny"},
+			{name: "North Carolina", code: "nc"},
+			{name: "North Dakota", code: "nd"},
+			{name: "Ohio", code: "oh"},
+			{name: "Oklahoma", code: "ok"},
+			{name: "Oregon", code: "or"},
+			{name: "Pennsylvania", code: "pa"},
+			{name: "Rhode Island", code: "ri"},
+			{name: "South Carolina", code: "sc"},
+			{name: "South Dakota", code: "sd"},
+			{name: "Tennessee", code: "tn"},
+			{name: "Texas", code: "tx"},
+			{name: "Utah", code: "ut"},
+			{name: "Vermont", code: "vt"},
+			{name: "Virginia", code: "va"},
+			{name: "Washington", code: "wa"},
+			{name: "West Virginia", code: "wv"},
+			{name: "Wisconsin", code: "wi"},
+			{name: "Wyoming", code: "wy"}
+		];
+
+		for (let i = 0; i < states.length; i ++) {
+			if ( states[i].name === state) {
+				stateParsed = states[i].code
+				console.log(stateParsed);
+			}
+		}
+
+		var stateQueryURL = "https://api.covidtracking.com/v1/states/" + stateParsed + "/current.json";
+
 		$.ajax({
 			url: stateQueryURL,
 			method: "GET"
@@ -98,8 +162,20 @@ $.ajax(hotelSettings).done(function (response) {
 			var activeCases = response.positive;
 			console.log("Active cases: " + activeCases);
 			var newCases = response.positiveIncrease;
-		    console.log("New cases: " + newCases);
-		}); 
+			console.log("New cases: " + newCases);
+
+			var countyQueryURL = "https://corona.lmao.ninja/v3/covid-19/jhucsse/counties/" + county;
+
+			$.ajax({
+				url: countyQueryURL,
+				method: "GET"
+			}).then(function(response) {
+				console.log(response);
+
+				var confirmedCases = response[0].stats.confirmed;
+				console.log("Confirmed cases: " + confirmedCases);
+			});
+		});
 	});
 	
 	})
@@ -115,3 +191,4 @@ $("#submitButton").on("click", function(event) {
 
     getCityInfo(city);
 })
+
